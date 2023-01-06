e5c4a344388669b19fd7154e22697c4290c12e87
smuratovic3 <smuratovic3@etf.unsa.ba>
2023-01-06 15:19:19 +0100

    get zahtjev za nastavnika

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const express = require("express"); // dodajemo expres
-const bodyParser = require("body-parser"); //paket body parser za parsiranje stringa u jason
-const app = express(); //pravimo insancu expresa
-const path = require("path"); // citanje iz patha
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const path = require("path");
 const nastavnici = require("./data/nastavnici.json");
-app.use(express.static(path.join(__dirname + "/public/html"))); //naglasavamo expresu koje staticke fajlove da korsitmo, sta sve mozemo korsitit
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname + "/public/html")));
 app.use(express.static(path.join(__dirname + "/public/css")));
 app.use(express.static(path.join(__dirname + "/public/ikone")));
 app.use(express.static(path.join(__dirname + "/public/scripts")));
-app.use(bodyParser.json()); // naglasavamo serveru da moze koristiti jason format
-app.use(bodyParser.urlencoded({ extended: true })); // da moze koristit afrikate
 
+//post zahtjev gdje se vrsi provjera da li korisnik postoji i da li je ispravna sifra
 app.post("/login/", function (req, res) {
-  console.log("Semina", req.body);
   let provjera = false;
   for (let i = 0; i < nastavnici.length; i++) {
     if (nastavnici[i].nastavnik.username === req.body.username) {
@@ -24,6 +25,21 @@ app.post("/login/", function (req, res) {
     res.status(200).json({ poruka: "Uspješna prijava" });
   } else {
     res.status(404).json({ poruka: "Neuspješna prijava" });
+  }
+});
+
+// get zahtjev za loginovanog nastavnika, ako nije loginovan poruka Nastavnik nije loginovan
+app.get("/predmeti/", function (req, res) {
+  let provjera = false;
+  for (let i = 0; i < nastavnici.length; i++) {
+    if (nastavnici[i].nastavnik.username === req.body.username) {
+      if (nastavnici[i].nastavnik.password_hash === req.body.password) {
+        provjera = true;
+      }
+    }
+  }
+  if (!provjera) {
+    res.status(404).json({ greska: "Nastavnik nije loginovan" });
   }
 });
 
